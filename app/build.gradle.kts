bda37983f458c38b0628e90ebdefa13bc7457d9c
Hugo <hugo.reyhol@educa.jcyl.es>
2025-02-11 21:12:02 +0100

    Cambio de visibilidad funcional

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.gestion_juegos_kotlin"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.gestion_juegos_kotlin"
--- app/src/main/java/com/example/gestion_juegos_kotlin/LoginActivity.kt
+++ app/src/main/java/com/example/gestion_juegos_kotlin/LoginActivity.kt
@@ -1,12 +1,16 @@
 package com.example.gestion_juegos_kotlin
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.InputType
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gestion_juegos_kotlin.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private var passIsVisible: Boolean = false
+    lateinit var passTypeface: Typeface
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +18,27 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initializeComponents()
+    }
+
+    private fun initializeComponents() {
+        passTypeface = binding.passwordEditText.typeface
+
+        binding.visibilityBtn.setOnClickListener {
+            passIsVisible = !passIsVisible;
+
+            if (passIsVisible) {
+                binding.visibilityBtn.setImageResource(R.drawable.ic_launcher_foreground) // TODO cambiar por ojo tachado cuando deje descargarlo
+                binding.passwordEditText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+
+            } else {
+                binding.visibilityBtn.setImageResource(R.drawable.baseline_visibility_24)
+                binding.passwordEditText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+
+            }
+
+            binding.passwordEditText.typeface = passTypeface
+            binding.passwordEditText.setSelection(binding.passwordEditText.text.length)
+        }
     }
 }
