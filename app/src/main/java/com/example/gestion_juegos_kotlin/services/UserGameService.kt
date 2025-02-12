ef11d301f288272728669e78db04129cc2fd53d1
Hugo <hugo.reyhol@educa.jcyl.es>
2025-02-12 18:21:20 +0100

    añadidos todos los metodos de UserGameService

--- app/src/main/java/com/example/gestion_juegos_kotlin/services/UserGameService.kt
+++ app/src/main/java/com/example/gestion_juegos_kotlin/services/UserGameService.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.example.gestion_juegos_kotlin.models.UserGame
 import com.example.gestion_juegos_kotlin.models.UserGameInsert
 import com.example.gestion_juegos_kotlin.models.UserGameResponse
+import com.example.gestion_juegos_kotlin.models.UserGameUpdate
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -57,7 +58,43 @@ object UserGameService {
         })
     }
 
-    fun deleteUserGame() {
+    fun updateUserGame(userGame: UserGame, update: UserGameUpdate) {
+        val token = UserService.user.token
+        val id = userGame.idGame
 
+        RetrofitClient.instance.updateUserGame(id, token, update).enqueue(object : Callback<Void> {
+            override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                if (response.isSuccessful) {
+                    // TODO actualizar interfaz y userGame
+
+                } else {
+                    Log.e("APIUserGames", "Error al actualizar juego de usuario")
+                }
+            }
+
+            override fun onFailure(call: Call<Void>, t: Throwable) {
+                Log.e("APIUserGames", t.message.toString())
+            }
+        })
+    }
+
+    fun deleteUserGame(userGame: UserGame) {
+        val token = UserService.user.token
+        val id = userGame.idGame
+
+        RetrofitClient.instance.deleteUserGame(id, token).enqueue(object : Callback<Void> {
+            override fun onResponse(call: Call<Void>, response: Response<Void>) {
+                if (response.isSuccessful) {
+                    // TODO actualizar interfaz
+
+                } else {
+                    Log.e("APIUserGames", "Error al borrar juego de usuario")
+                }
+            }
+
+            override fun onFailure(call: Call<Void>, t: Throwable) {
+                Log.e("APIUserGames", t.message.toString())
+            }
+        })
     }
 }
