cb86e9cf46d9823e6c338fb4e1db332a46df4a6d
Hugo <hugo.reyhol@educa.jcyl.es>
2025-02-12 19:42:56 +0100

    Hecho toolbar

--- app/src/main/java/com/example/gestion_juegos_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/gestion_juegos_kotlin/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.gestion_juegos_kotlin
 
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.gestion_juegos_kotlin.databinding.ActivityMainBinding
+import com.example.gestion_juegos_kotlin.models.UserGame
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -20,5 +26,41 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initializeComponents()
+    }
+
+    private fun initializeComponents() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.title = ""
+
+        val options = UserGame.Companion.GameStates.entries.toTypedArray().toMutableList()
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, options)
+        binding.spinner.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_home, menu)
+
+        val searchItem: MenuItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                Log.i("SearchBar", "$p0")
+                // TODO implementar busqueda
+                return false
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                Log.i("SearchBar", "$p0")
+                // TODO implementar busqueda
+                return false
+            }
+
+        })
+
+
+        return true
     }
 }
