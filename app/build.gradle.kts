7d6308fa90b9c5b57d1b6043067915925a0256e7
Hugo <hugo.reyhol@educa.jcyl.es>
2025-02-12 12:12:20 +0100

    Añadidas dependencias API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    // API
+    implementation(libs.retrofit)
+    implementation(libs.gson)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
