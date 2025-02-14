b04dcd710bb815d14cf98d101938f81d48fdfe1e
Hugo <hugo.reyhol@educa.jcyl.es>
2025-02-14 12:36:18 +0100

    Empezados Details y CollectionFragment

--- app/src/main/java/com/example/gestion_juegos_kotlin/CollectionFragment.kt
+++ app/src/main/java/com/example/gestion_juegos_kotlin/CollectionFragment.kt
@@ -1,22 +1,44 @@
 package com.example.gestion_juegos_kotlin
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.gestion_juegos_kotlin.databinding.FragmentCollectionBinding
 
 class CollectionFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    lateinit var binding: FragmentCollectionBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_collection, container, false)
+        binding = FragmentCollectionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        inicializeRecycler()
+    }
+
+    private fun inicializeRecycler() {
+        binding.collectioRecycler.layoutManager = GridLayoutManager(context, 2)
+        TODO("Añadir Adaptador de colección")
+        val adapter = CollectionAdapter() {onClickOpenDetails()}
+        binding.collectioRecycler.adapter = adapter
+
+    }
+
+    private fun onClickOpenDetails() {
+        TODO("Hacer clase Details")
+        startActivity(
+            Intent(context, Details::class.java)
+        )
     }
 }
