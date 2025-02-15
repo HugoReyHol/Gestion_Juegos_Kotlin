dd59833ffe91f50bfd5419451a2666ee14101346
Hugo <hugo.reyhol@educa.jcyl.es>
2025-02-15 01:08:50 +0100

    Añadido nuevo fragmento y lógica navbar

--- app/src/main/java/com/example/gestion_juegos_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/gestion_juegos_kotlin/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.SearchView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.example.gestion_juegos_kotlin.databinding.ActivityMainBinding
 import com.example.gestion_juegos_kotlin.models.UserGame
+import com.example.gestion_juegos_kotlin.util.GamesFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -38,6 +42,25 @@ class MainActivity : AppCompatActivity() {
         options.add("TODOS")
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, options)
         binding.spinner.adapter = adapter
+
+        binding.bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.collection_nav -> {
+                    supportFragmentManager.commit {
+                        replace(R.id.fragmentContainerView, CollectionFragment())
+                    }
+                    binding.spinner.visibility = View.VISIBLE
+                }
+                R.id.game_nav -> {
+                    supportFragmentManager.commit {
+                        replace(R.id.fragmentContainerView, GamesFragment())
+                    }
+                    binding.spinner.visibility = View.INVISIBLE
+                }
+            }
+
+            true
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
